25d0378ecb6039ffd8e2eb06c862ea32e6d739c6
Not-Leader <notleader3@gmail.com>
2020-12-10 22:33:20 +0300

    added scalability to blank window

--- main.rkt
+++ main.rkt
@@ -1,7 +1,9 @@
 #lang racket/gui
 (define main-window (new frame%
                          [label "Welcome to NFTT!"]
-                         [width 1200]
-                         [height 1200]))
+                         [width 800]
+                         [height 700]
+                         [stretchable-width 1200]
+                         [stretchable-height 1200]))
 
 (send main-window show #t)
